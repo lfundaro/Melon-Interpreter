8c90c18ec8505158d30d3148aae1aee9882e5cc5
unknown <Mundos Graves@.(none)>
2009-12-05 18:09:55 -0400

    empezando el match

--- interpreter.py
+++ interpreter.py
@@ -2,6 +2,15 @@ import re
 
 def match(n1, n2):
     if (re.match(n1.tipo,'BOOLEANO') and re.match(n2.tipo,'BOOLEANO')):
+		if (re.match(n1.hijo,'TRUE') and re.match(n2.hijo,'TRUE')):
+			return true
+		elif (re.match(n1.hijo, 'FALSE') and re.match(n2.hijo,'FALSE'):
+			return true
+		elif: 
+			return false
+	elif (re.match(n1.tipo,'LISTAVACIA') and re.match(n2.tipo, 'LISTAVACIA'):
+		return true
+	
         
     
     
